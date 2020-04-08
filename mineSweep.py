3db1769ecc3a002f64ff4e5204d79415181ff4d1
yingjie <yingjie_qiao@outlook.com>
2020-04-08 22:19:44 +0800

    UI basic frame done. Mine field done. Algo to do

--- mineSweep.py
+++ mineSweep.py
@@ -1,14 +1,18 @@
-import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.interactive import InteractiveLauncher
-from kivy.uix.textinput import TextInput
+from kivy.uix.textinput import TextInput #name for scoreboard
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button 
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window 
+from kivy.properties import NumericProperty
+from kivy.event import EventDispatcher
+from random import randint
+
 
 ##TODO: position and size
 ##understand layouts
@@ -17,22 +21,40 @@ from kivy.core.window import Window
 class MineSweep(App):
     def build(self):
         sm = ScreenManager()
+        
         menu = Menu(name="menu")
         options = Options(name="options")
-        game = Game(name="game")
+        gamemode = GameMode(name="gamemode")
+        easy = Easy(name="easy")
+        medium = Medium(name="medium")
+        hard = Hard(name="hard")
+        customize = Customize(name="customize")
+        
         sm.add_widget(menu)
         sm.add_widget(options)
-        sm.add_widget(game)
+        sm.add_widget(gamemode)
+        sm.add_widget(easy)
+        sm.add_widget(medium)
+        sm.add_widget(hard)
+        sm.add_widget(customize)
         sm.current = "menu"
         return sm
     
+class MyButton(Button,EventDispatcher):
+    isMine = NumericProperty(0)
+    def __init__(self, **kwargs):
+        #super(MyButton,self).__init__(**kwargs)
+        Button.__init__(self,**kwargs)
+        
+    
+
 class Menu(Screen,GridLayout):
     def __init__(self, **kwargs):
         Screen.__init__(self, **kwargs)
         GridLayout.__init__(self, **kwargs)
         
         self.layout = GridLayout(cols=1)
-        self.start_button = Button(text="Start Game", on_press=self.start_game)
+        self.start_button = Button(text="Start Game", on_press=self.change_to_gamemode)
         self.options_button = Button(text="Options", on_press=self.change_to_options)
         self.quit_button = Button(text="Quit", on_press=self.quit_app)
         
@@ -42,10 +64,10 @@ class Menu(Screen,GridLayout):
         
         self.add_widget(self.layout)
         
-    def start_game(self, value):
+    def change_to_gamemode(self, value):
         self.manager.transition.direction = "up"
-        self.manager.current = "game"
-        
+        self.manager.current = "gamemode"
+
     def change_to_options(self, value):
         self.manager.transition.direction = "up"
         self.manager.current = "options"
@@ -62,6 +84,7 @@ class Options(Screen,GridLayout):
         self.layout = GridLayout(cols=1)
         self.options_label = Label(text="Options")
         self.menu_button = Button(text="Back to Menu", on_press=self.change_to_menu)
+        
         self.layout.add_widget(self.options_label)
         self.layout.add_widget(self.menu_button)
         self.add_widget(self.layout)
@@ -70,22 +93,103 @@ class Options(Screen,GridLayout):
         self.manager.transition.direction = "down"
         self.manager.current = "menu"
         
-class Game(Screen,GridLayout):
+class GameMode(Screen,GridLayout):
     def __init__(self, **kwargs):
         Screen.__init__(self, **kwargs)
         GridLayout.__init__(self, **kwargs)
                 
         self.layout = GridLayout(cols=1)
-        ## TODO
-        self.game_label = Label(text="Game")
+        self.gamemode_label = Label(text="Select Difficulty")
+        self.easy_button = Button(text="Easy", on_press=self.change_to_easy)
+        self.medium_button = Button(text="Medium", on_press=self.change_to_medium)
+        self.hard_button = Button(text="Hard", on_press=self.change_to_hard)
+        self.customize_button = Button(text="Customized Game", on_press=self.change_to_customize)
         self.menu_button = Button(text="Back to Menu", on_press=self.change_to_menu)
-        self.layout.add_widget(self.game_label)
+        
+        self.layout.add_widget(self.gamemode_label)
+        self.layout.add_widget(self.easy_button)   
+        self.layout.add_widget(self.medium_button)
+        self.layout.add_widget(self.hard_button)
+        self.layout.add_widget(self.customize_button)
         self.layout.add_widget(self.menu_button)
         self.add_widget(self.layout)
-        
+
     def change_to_menu(self, value):
         self.manager.transition.direction = "down"
         self.manager.current = "menu"
+        
+    def change_to_easy(self, value):
+        self.manager.transition.direction = "down"
+        self.manager.current = "easy"
+        
+    def change_to_medium(self, value):
+        self.manager.transition.direction = "down"
+        self.manager.current = "medium"
+        
+    def change_to_hard(self, value):
+        self.manager.transition.direction = "down"
+        self.manager.current = "hard"
+        
+    def change_to_customize(self, value):
+        self.manager.transition.direction = "down"
+        self.manager.current = "customize"
+
+
+class Easy(Screen,FloatLayout):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        FloatLayout.__init__(self, **kwargs)
+        
+        self.layout = FloatLayout(size=(300, 300))
+        coordinates = {}
+        mines = 10
+        mineTotal = 0
+        
+        # TODO
+        # position and sizes to be modified to create a better UI
+        for x in range(10):
+            for y in range(10):
+                ifMine = randint(0,1)
+                if ifMine and mineTotal < mines:
+                    self.btn = MyButton(isMine=1,size_hint=(10/300,10/300),pos=(x*30,y*30))
+                    self.layout.add_widget(self.btn)
+                    mineTotal += 1
+                    coordinates[(x*30,y*30)] = 1
+                    #print(self.btn.isMine)
+                else:
+                    self.btn = MyButton(isMine=0,size_hint=(10/300,10/300),pos=(x*30,y*30))
+                    self.layout.add_widget(self.btn)
+                    coordinates[(x*30,y*30)] = 0
+                    #print(self.btn.isMine)
+        self.add_widget(self.layout)
+        
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            if Clock.get_time() - touch.time_start > TOUCH_HOLD_THRESHOLD:
+                self.toggle_guess_bomb()
+            else:
+                self.reveal_square()
+            return True
+        
+        
+
+
+class Medium(Screen,GridLayout):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        GridLayout.__init__(self, **kwargs)
+
+
+class Hard(Screen,GridLayout):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        GridLayout.__init__(self, **kwargs)
+     
+        
+class Customize(Screen,GridLayout):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        GridLayout.__init__(self, **kwargs)
 
 if __name__ =='__main__':
     MineSweep().run()
